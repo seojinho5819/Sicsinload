92a34fd9f4ff0aa33d9df4f5810ee9c4d65b56a0
soejinho <jhseo@korens.co.kr>
2022-07-21 17:51:57 +0900

    2022-07-21작업물

---
+++ sicsinload/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Maps from './Maps';
+
+
+
+function App(){
+  
+  return (
+    <div className="App">
+      <div className='map'>
+        <Maps/>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
