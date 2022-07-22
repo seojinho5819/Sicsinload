6caf0b3b679fec30eac56240aa954dcf29d8b47b
soejinho <jhseo@korens.co.kr>
2022-07-22 11:30:42 +0900

    -

--- sicsinload/src/App.js
+++ sicsinload/src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
-import Maps from './Maps';
 
+const { naver } = window;
+function App() {
 
+  let mapOptions = {
+    center: new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 17
+};
 
-function App(){
-  
+  let map = new naver.maps.Map('map', mapOptions);
+  const markser = new naver.maps.Marker({
+    map,
+    position: new naver.maps.LatLng(37.3595704, 127.105399),
+  });
   return (
     <div className="App">
-      <div className='map'>
-        <Maps/>
-      </div>
+     <div id='map' style={{width:'400px',height:'400px'}}></div>
     </div>
-    
   );
 }
 
